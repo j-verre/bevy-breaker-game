215873607926d7dc6e39912b17cdaccb888fe4c3
jverre <jeff.verre@lexialearning.com>
2024-12-27 09:18:16 -0500

    add window title

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ use systems::{audio::play_background_music, initialize, ui::create_paddle};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "\"Breaker!\"".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, initialize)
         .add_systems(Startup, create_paddle)
         .add_systems(PostStartup, play_background_music)
